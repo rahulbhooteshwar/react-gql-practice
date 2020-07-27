5b2f2a37cc93c19d3beefcf42c94e4c23ec1df76
Rahul Bhooteshwar <rahul.bhooteshwar@gmail.com>
2020-07-27 14:25:57 +0530

    Updates

---
+++ precache-manifest.e43114fde95aee63cc952cb349f0c5d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a388054793763dbaa7acb4f3188225",
+    "url": "/react-gql-practice/index.html"
+  },
+  {
+    "revision": "657f1b4d4b90786eaca3",
+    "url": "/react-gql-practice/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "b40b5a5702f4b8933c76",
+    "url": "/react-gql-practice/static/css/main.7cbd37f8.chunk.css"
+  },
+  {
+    "revision": "657f1b4d4b90786eaca3",
+    "url": "/react-gql-practice/static/js/2.eeeb3c2e.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/react-gql-practice/static/js/2.eeeb3c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40b5a5702f4b8933c76",
+    "url": "/react-gql-practice/static/js/main.ac64a12b.chunk.js"
+  },
+  {
+    "revision": "c12009e43e7ebbe32bf9",
+    "url": "/react-gql-practice/static/js/runtime-main.ee9b7cc2.js"
+  }
+]);
